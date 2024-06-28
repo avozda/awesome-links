da8f2876b45137e3b405e392632a81bf1cf12f05
adamvozda <adam.vozda@b2a.cz>
2024-06-28 14:12:17 +0200

    remove redundant files

--- graphql/resolvers.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "../lib/prisma";
-
-
-export const resolvers = {
-    Query: {
-        links: async (_parent: any, _args: any, context: any) => await context.prisma.link.findMany()
-    }
-};
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,30 +1,17 @@
-import { ApolloServer } from "apollo-server-micro";
-import { schema } from "../../graphql/schema";
-import { resolvers } from "../../graphql/resolvers";
-import Cors from "micro-cors";
-import { createContext } from "../../graphql/context";
-const cors = Cors();
-const apolloServer = new ApolloServer({
+import { createYoga } from 'graphql-yoga'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { schema } from '../../graphql/schema'
+
+export default createYoga<{
+    req: NextApiRequest
+    res: NextApiResponse
+}>({
     schema,
-    resolvers,
-    context: createContext
-});
-
-const startServer = apolloServer.start();
-
-export default cors(async function handler(req: any, res: any) {
-    if (req.method === "OPTIONS") {
-        res.end();
-        return false;
-    }
-    await startServer;
-    await apolloServer.createHandler({
-        path: "/api/graphql",
-    })(req, res);
-});
+    graphqlEndpoint: '/api/graphql'
+})
 
 export const config = {
     api: {
-        bodyParser: false,
+        bodyParser: false
     }
 }
